bffae3c0ef23eeaf519ff7a9ba279b362e6e253a
celine <ctran@et.esiea.fr>
2021-12-11 18:41:50 +0100

    Points Light + Fairy Light Blinking Menu

---
+++ Assets/Scripts/Scripts-Start/PointLight.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointLight : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_LightG1 = null;
+    [SerializeField]
+    private GameObject m_LightG2 = null;
+    [SerializeField]
+    private GameObject m_LightG3 = null;
+
+    [SerializeField]
+    private GameObject m_LightR1 = null;
+    [SerializeField]
+    private GameObject m_LightR2 = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LightBlink());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator LightBlink()
+    {
+        bool m_LightON = true;
+
+        while (true)
+        {
+            m_LightG1.gameObject.SetActive(m_LightON);
+            m_LightG2.gameObject.SetActive(m_LightON);
+            m_LightG3.gameObject.SetActive(m_LightON);
+            m_LightR1.gameObject.SetActive(!m_LightON);
+            m_LightR2.gameObject.SetActive(!m_LightON);
+            m_LightON = !m_LightON;
+            yield return new WaitForSeconds(1f);
+        }
+        yield return 0;
+    }
+}
